e4ac1a7b190626f8bbb84a996e31a27bfe157800
upamasaki <m.tani.july@gmail.com>
2020-10-02 23:35:31 +0900

    add comment

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # stock_ai
 
 
+### 仮想環境の作成
+```
+conda create -n stock_venv python=3.7.5
+```
+
 ### 仮想環境に入る
 ```
 conda activate stock_venv
 ```
 
+### パッケージのアップデート
+```
+python -m pip install --upgrade pip
+```
 
 ### 必要パッケージのインストール
 ```
 pip install requests
 pip install pandas
+pip install tqdm
+pip install seaborn
+```
 
+### 仮想環境の削除
+```
+conda remove -n myenv --all
 ```
--- main.py
+++ main.py
@@ -12,19 +12,40 @@ import matplotlib.pyplot as plt
 sns.set_theme(style="darkgrid")
 
 def main():
-    # preprocess()
-    visual()
-
-def download():
-
+    # 株の名前
     stock_name = "AAPL"
+
+    #############################################
+    # 株データの生データのダウンロード
+    #
+    # download(stock_name)
+
+    #############################################
+    # 生データの前処理(UNIX時刻からの変換)
+    #
+    # preprocess(stock_name)
+
+    #############################################
+    # 描画(seabornを使用)
+    #
+    visual(stock_name)
+
+def download(stock_name):
+
+    # request URL
     URL = "https://query1.finance.yahoo.com/v7/finance/chart/{}?range=1d&interval=1m&indicators=quote&includeTimestamps=true".format(stock_name)
+
+    # データの保存path
     csvpath = "./output/stock_origin_{}.csv".format(stock_name)
 
+    # request
     r = requests.get(URL)
     s = StringIO(r.text)
+
+    # json の取得
     j = json.load(s)
 
+    # jsonからdataframeへの変換
     df = pd.DataFrame()
     df['timestamp'] = j['chart']['result'][0]['timestamp']
     df['open'] = j['chart']['result'][0]['indicators']['quote'][0]['open']
@@ -33,55 +54,58 @@ def download():
     df['close'] = j['chart']['result'][0]['indicators']['quote'][0]['close']
     df['volume'] = j['chart']['result'][0]['indicators']['quote'][0]['volume']
 
+    # csvの保存
     df.to_csv(csvpath, index=False, encoding='utf8')
 
-def preprocess():
-    df_read = pd.read_csv('./output/out.csv')
+def preprocess(stock_name):
+
+    # 読み込みデータのpath
+    csvpath = "./output/stock_origin_{}.csv".format(stock_name)
+
+    # 生データの読み込み
+    df_read = pd.read_csv(csvpath)
     print(df_read)
 
-    now = datetime.now()
-    print(now)
-
-    now_from_ts = datetime.fromtimestamp(1601386200)
-    print(now_from_ts)
-
+    # 株の名前
     stock_name = "AAPL"
 
-    csvpath = './output/out2.csv'
+    # データの保存path
+    csvpath = "./output/stock_{}.csv".format(stock_name)
+
+    # 一行ずつ時刻の変換
     for i in range(len(df_read)):
         df_read.loc[i, 'time'] = datetime.fromtimestamp(df_read.loc[i, 'timestamp'])
         df_read.loc[i, 'stock_name'] = stock_name
+
+    # 結果の出力
     print(df_read)
+
+    # csvの保存
     df_read.to_csv(csvpath, index=False, encoding='utf8')
 
-def visual():
-
+def visual(stock_name):
+    # 読み込みデータのpath
+    csvpath = "./output/stock_{}.csv".format(stock_name)
 
     # Load an example dataset with long-form data
-    df = pd.read_csv('./output/out2.csv')
+    df = pd.read_csv(csvpath)
 
+    # 図のサイズ指定
     fig = plt.figure(figsize=(30, 10))
+
+
     # Plot the responses for different events and regions
     sns.lineplot(x="time",
                  y="open",
                  hue="stock_name",
                  data=df)
-    # sns.lineplot(x="time",
-    #              y="close",
-    #              hue="stock_name",
-    #              data=df)
-    plt.savefig("seaborntest.png")  # save as png file
 
-    # # Load an example dataset with long-form data
-    # fmri = sns.load_dataset("fmri")
-    # print(fmri)
-    #
-    # fig = plt.figure() #わからない2
-    # # Plot the responses for different events and regions
-    # sns.lineplot(x="timepoint", y="signal",
-    #              hue="region", style="event",
-    #              data=fmri)
-    # plt.savefig("seaborntest2.png")  # save as png file
+    # 図の保存path
+    figpath = "./output/stock_{}_fig.png".format(stock_name)
+
+    # 図の保存
+    plt.savefig(figpath)  # save as png file
+
 
 if __name__ == '__main__':
     main()
